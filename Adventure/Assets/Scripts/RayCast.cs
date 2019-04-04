1074854ef94230d0eb8016ccec306b0cc9e2091a
CamHargrave <tvastacarious@gmail.com>
2019-04-04 18:18:23 -0500

    Lots Added
    Scripts added in

---
+++ Adventure/Assets/Scripts/RayCast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    [Tooltip("Starting point f raycast used to detect interatives.")]
+
+    [SerializeField]
+    private Transform rayCastOrigin;
+    [Tooltip("How far raycast will travel to search for interactive objects.")]
+    [SerializeField]
+    private float maxDistance;
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(rayCastOrigin.position, rayCastOrigin.forward * maxDistance, Color.red);
+        RaycastHit hitInfo;
+        bool objectwasDetected = Physics.Raycast(rayCastOrigin.position, rayCastOrigin.forward, out hitInfo , maxDistance);
+
+        IInteractive interactive = null;
+
+        if (objectwasDetected)
+        {
+            //Debug.Log($"Player is looking at {hitInfo.collider.gameObject.name} ");
+            interactive = hitInfo.collider.gameObject.GetComponent<IInteractive>();
+        }
+
+        if (interactive != null)
+        {
+            interactive.InteractWith();
+        }
+    }
+
+}
