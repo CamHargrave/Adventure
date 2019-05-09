cbcd7df3f04557490d894c05ce8d625fad82d5d3
CamHargrave <tvastacarious@gmail.com>
2019-05-09 00:40:53 -0500

    Finished
    Added everything that I can added

---
+++ Adventure/Assets/Scripts/OutofSceneDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutofSceneDoor : InteractiveObject
+{ 
+
+    [SerializeField]
+    private string gameSceneName;
+
+    public OutofSceneDoor()
+    {
+        displayText = nameof(Door);
+    }
+
+    protected override void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    public override void InteractWith()
+    {
+        audioSource.Play();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(gameSceneName);
+
+    }
+}
---
+++ Adventure/Assets/Scripts/TransitionDoors.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionDoors : InteractiveObject
+{
+    [SerializeField]
+    private string gameSceneName;
+
+    public TransitionDoors()
+    {
+        displayText = nameof(Door);
+    }
+
+    protected override void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+       
+    }
+
+    public override void InteractWith()
+    {
+        audioSource.Play();
+        SceneManager.LoadScene(gameSceneName);
+
+    }
+
+}
